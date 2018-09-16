04a795852e48dce1bbe55573f6086921fdfb248c
Dương Trung Kiên <kiendt@vnpt-technology.vn>
2018-09-16 15:39:45 +0700

    build basic project spring security- spring boot

---
+++ BasicSpringbootProject/src/main/java/com/vnpt/ssdc/SecurityConfig.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vnpt.ssdc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *
+ * @author soithattha
+ * @date Sep 15, 2018
+ * @version
+ * @description
+ */
+//@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("soithattha").password("123456").roles("USER");
+//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
+
+    }
+
+}

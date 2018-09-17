b4eaf2ff8b144665f2d883419162b48df6fdd733
Dương Trung Kiên <kiendt@vnpt-technology.vn>
2018-09-17 23:38:04 +0700

    add basic project and spring-security with UserService

---
+++ spring-security/src/main/java/com/vnpt/ssdc/controller/HomeController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vnpt.ssdc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author kiendt
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String login() {
+        return "home";
+    }
+}
